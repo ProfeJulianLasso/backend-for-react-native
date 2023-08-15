ee2484d3ab73e8f5275c128c60218eecfa4dd191
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-15 14:35:07 -0500

    fix(infrastructure): :bug: se corrige la importación de AppModule en el archivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { AppModule } from '@infra';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
