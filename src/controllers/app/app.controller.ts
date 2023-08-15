b5f5de0656e6f131265f429644b709972df0cc94
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-15 09:21:01 -0500

    feat: :sparkles: orden de estructura inicial

---
+++ src/controllers/app/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { AppService } from '../../services/app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  getHello1(): string {
+    return this.appService.getHello();
+  }
+
+  @Put()
+  getHello2(): string {
+    return this.appService.getHello();
+  }
+
+  @Patch()
+  getHello3(): string {
+    return this.appService.getHello();
+  }
+
+  @Delete()
+  getHello4(): string {
+    return this.appService.getHello();
+  }
+}
