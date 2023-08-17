eda8e3548859691e2db9d78ddcc1a46239192337
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-17 09:02:17 -0500

    fix: :bug: configuración de arranque de microservicio

--- apps/micro1/src/main.ts
+++ apps/micro1/src/main.ts
@@ -3,13 +3,22 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { Micro1Module } from './micro1.module';
 
 async function bootstrap() {
-  await NestFactory.createMicroservice<MicroserviceOptions>(Micro1Module, {
+  // NestFactory.createMicroservice<MicroserviceOptions>(Micro1Module, {
+  //   transport: Transport.TCP,
+  //   options: {
+  //     port: 3001,
+  //   },
+  // });
+  // await app.startAllMicroservices();
+
+  const app = await NestFactory.create(Micro1Module);
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
       port: 3001,
     },
   });
-  // await app.startAllMicroservices();
+  await app.startAllMicroservices();
   console.log(`🚀 Microservice is running on port 3001`);
 }
 bootstrap();
